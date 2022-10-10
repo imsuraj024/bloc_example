c1b6d1e2b22e98470db60c1d143f29a58b37f6a3
suraj-official <suraj.mishra@sowlab.com>
2022-10-11 00:01:13 +0530

    Check Internet Connectivity

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bloc_example/blocs/internet_bloc/internet_bloc.dart';
+import 'package:bloc_example/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:bloc_example/blocs/internet_bloc/internet_bloc.dart';
+import 'package:bloc_example/blocs/internet_bloc/internet_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child: BlocConsumer<InternetBloc,InternetState>(
+              listener: (context, state) {
+                 if (state is InternetGainedState){
+                   ScaffoldMessenger.of(context).showSnackBar(
+                     SnackBar(
+                       content: Text("Internet Connected!"),
+                     backgroundColor: Colors.green,)
+                   );
+                 } else if (state is InternetLostState) {
+                   ScaffoldMessenger.of(context).showSnackBar(
+                       SnackBar(
+                         content: Text("Internet Disconnected!"),
+                         backgroundColor: Colors.red,)
+                   );
+                 }
+              },
+              builder: (context,state) {
+                if(state is InternetGainedState){
+                  return Text("Connected!");
+                } else if (state is InternetLostState) {
+                  return Text("Not Connected!");
+                } else {
+                  return Text("Loading...");
+                }
+              },
+
+            ),
+
+            /*child: BlocBuilder<InternetBloc, InternetState>(
+              builder: (context, state) {
+                if(state is InternetGainedState){
+                  return Text("Connected!");
+                } else if (state is InternetLostState) {
+                  return Text("Not Connected!");
+                } else {
+                  return Text("Loading...");
+                }
+              }),*/
+            ),
+          ),
+        ),
+      );
+  }
+}
